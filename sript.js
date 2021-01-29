1ff9e532720cb71aeb7e5d8302c927b847d95e54
FazleRabbi <24.fazlerabbi@gmail.com.com>
2021-01-29 05:13:08 -0600

    button modified

--- sript.js
+++ sript.js
@@ -6,10 +6,25 @@ function btn(val) {
     document.getElementById('input').value=inpu + text;
 }
 
-
-function solution() {
-    const inpu = document.getElementById('input').value;
-    document.getElementById('input').value=eval(inpu);
+function less(val) {
+    const input = document.getElementById('input').value;
+    document.getElementById('input').value=input.slice(0, input.length-1);
+    console.log(val.value)
 }
 
+function solution() {
+    const input = document.getElementById('input').value;
+    document.getElementById('input').value=eval(input);
+}
+function remove() {
+    const input = document.getElementById('input').value;
+    document.getElementById('input').value="";
+}
+// function operation(type) {
+
+
+
+//     const input = document.getElementById('input').value;
+//     document.getElementById('input').value="";
+// }
 
